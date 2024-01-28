5ef8e4eab240b640e66d155f54f2be9879cb17e0
Michal Stachura <gritoo@gmail.com>
2024-01-28 20:33:06 +0100

    dziala pobieranie danych przezz api

--- DataInteract/Program.cs
+++ DataInteract/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddSingleton<PizzaService>();
+builder.Services.AddHttpClient<ApiClientService>();
 
 var app = builder.Build();
 
---
+++ DataInteract/Services/ApiClientService.cs
@@ -0,0 +1,15 @@
+﻿using DataInteractShared;
+
+namespace DataInteract.Services
+{
+    public class ApiClientService(HttpClient httpClient)
+    {
+        private readonly string _specialsUrl = "https://localhost:7064/specials";
+        public async Task<List<PizzaSpecial>> DajPizzaSpecials()
+        {
+            return await httpClient.GetFromJsonAsync<List<PizzaSpecial>>(_specialsUrl) ?? [];
+        }
+
+
+    }
+}
